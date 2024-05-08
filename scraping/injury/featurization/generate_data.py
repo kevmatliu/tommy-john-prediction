74f6fc7a7e6d68db49cd6f51091ddbacca7cda56
Kevin Liu <kmliu@college.harvard.edu>
2024-05-08 17:56:27 -0400

    sampled training and testing data from injury.csv

---
+++ scraping/injury/featurization/generate_data.py
@@ -0,0 +1,33 @@
+import random
+import pandas as pd
+import json
+
+
+def generate_data(type_of_data, num):
+    df = pd.read_csv("../../data/injury.csv")
+    df = df[df["relinquished"].notnull()]
+
+    data = []
+
+    for _ in range(num):
+        row = df.sample()
+        if any([row["notes"].values[0] in datum["text"] for datum in data]):
+            continue
+
+        data.append({
+            "text": row["notes"].values[0],
+        })
+    
+    if type_of_data == "training":
+        with open("training_data.json", "w") as f:
+            json.dump(data, f, indent=4)
+    elif type_of_data == "test":
+        with open("test_data.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+
+if __name__ == "__main__":
+    generate_data("test", 40)
+    
+
+    
